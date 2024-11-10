7a149948d5acfd0a1a4485820cd335f7fb236340
ulian <ulian@JULY>
2024-11-10 16:39:46 +0200

    Добавьте файлы проекта.

---
+++ Labolatorna3task1/Labolatorna3task1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double a, b, c;
+    cout << "Введіть значення a: ";
+    cin >> a;
+    cout << "Введіть значення b: ";
+    cin >> b;
+    cout << "Введіть значення c: ";
+    cin >> c;
+
+    
+    if (b - a <= 0) {
+        cout << "Помилка: b - a повинно бути більше за нуль для кореня." << endl;
+        return 1;
+    }
+
+  
+    double vuraz1 = sqrt(b / sqrt(b - a)) / (b * b + a * b * c);
+    double dodatoc = abs(b + a);
+    double f = vuraz1 + dodatoc;
+
+  
+    cout << "Значення f: " << f << endl;
+
+    return 0;
+}
